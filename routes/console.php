e916f1899ac92a8ba6d9d4db473d8c1aae06dc41
Pim vd Molen <info@pimvdmolen.nl>
2026-09-19 00:00:25 +0200

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Voorkom dat scheduler.log onbeperkt groeit: verwijder het wekelijks (maandag 02:00).
+Schedule::call(function () {
+    $log = storage_path('logs/scheduler.log');
+
+    if (is_file($log)) {
+        @unlink($log);
+    }
+})->weeklyOn(1, '02:00')
+    ->timezone('Europe/Amsterdam')
+    ->name('cleanup-scheduler-log');
